3ecdc40de70431ba1c3612cae0be72831f1ce258
Divyagash <sdivyagash@yahoo.com>
2018-09-28 10:52:23 -0400

    Updated the NEWS file and the R scripts

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -2,5 +2,11 @@
 ## BAM files were converted back to FASTQ files in FireCloud
 # browseURL("https://software.broadinstitute.org/firecloud/")
 
+## BAM files were converted using bamtofastq function in bedtools
+
 ## FASTQ files were aligned and processed in FireCloud methylation pipeline
 # browseURL("https://portal.firecloud.org/#workspaces/aryee-merkin/TCGA_WGBS_hg19")
+
+## First, 3 base pairs from both end of the reads were trimmed using trimgalore
+
+## Then the FASTQ files were aligned to the hg19 genome using bismark with default parameters
